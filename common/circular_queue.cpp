9e9414554f5c34cc588a8051102cab9380126631
sherlockzhang <sherlockzhang@123u.com>
2018-04-12 21:49:25 +0800

    circular_queue

--- common/circular_queue.cpp
+++ common/circular_queue.cpp
@@ -1,19 +1,110 @@
 #include "circular_queue.h"
 
-CircularQueue()
+#include <cstring.h>
+
+CircularQueue::CircularQueue(uint32_t size)
+	:m_buffer(NULL)
+	,m_size(0)
+	,m_head(0)
+	,m_tail(0)
 {
+	Init(size);
 }
 
-~CircularQueue()
+CircularQueue::~CircularQueue()
 {
+	UnInit();
 }
 
-bool CircularQueue::init()
+bool CircularQueue::Init(uint32_t size)
 {
-    return false;
+	m_buffer = new char[size];
+	if (NULL == m_buffer)
+	{
+		return false;
+	}
+	m_size = size;
+	m_head = 0;
+	m_tail = 0;
+
+	return true;
 }
 
-void CircularQueue::unInit()
+void CircularQueue::UnInit()
 {
+	delete[] m_buffer;
+	m_buffer = NULL;
+	m_size = 0;
+	m_head = 0;
+	m_tail = 0;
+}
+
+
+bool CircularQueue::Write(char* str, uint32_t len)
+{
+	if (NULL == str || 0 == len)
+	{
+		return false;
+	}
+	if (Left() < len)
+	{
+		return false;
+	}
+
+	if (m_tail + len < m_size)		
+	{
+		memcpy(TailPtr(), str, len);
+		m_tail += len;
+	}
+	else
+	{
+		uint32_t seg1 = m_size - m_tail;
+		uint32_t seg2 = len - seg2;
+		memcpy(TailPtr(), str, seg1);
+		memcpy(m_buffer, str + seg1, seg2);
+		m_tail = m_buffer + seg2;
+	}
+}
+
+bool CircularQueue::Read(char* str, uint32_t len)
+{
+	if (NULL == str || 0 == len)
+	{
+		return false;
+	}
+	if (Used() < len)
+	{
+		return false;
+	}
+	if (m_tail >= m_head)
+	{
+		memcpy(str, HeadPtr(), len);
+		m_head += len;
+	}
+	else
+	{
+		uint32_t seg1 = len - m_tail;
+		uint32_t seg2 = len - seg1;
+		memcpy(str, HeadPtr(), seg1);
+		memcpy(str + seg1, m_buffer, seg2);
+		m_head = m_buffer + seg2;
+	}
+}
+
+uint32_t CircularQueue::Used()
+{
+	if (m_tail >= m_head)
+	{
+		return m_tail - m_head;
+	}
+	else
+	{
+		return m_tail + m_size - m_head;
+	}
+}
+
+uint32_t CircularQueue::Left()
+{
+	return m_size - Used() - 1;
 }
 
--- common/circular_queue.h
+++ common/circular_queue.h
@@ -1,25 +1,32 @@
 #ifndef __CIRCULAR_QUEUE_H__
 #define __CIRCULAR_QUEUE_H__
 
+#include <stdint.h>
+
 class CircularQueue
 {
-    public:
-        CircularQueue();
-        ~CircularQueue();
+public:
+	CircularQueue(uint32_t size);
+	~CircularQueue();
 
-        bool init();
-        void unInit();
+	bool Init(uint32_t size);
+	void UnInit();
 
-        bool write(char* pStr, uint32_t len);
-        bool read(char* pStr, uint32_t len);
+	bool Write(char* str, uint32_t len);
+	bool Read(char* str, uint32_t len);
 
-        uint32_t left();
+private:
+	uint32_t Used();
+	uint32_t Left();
 
-    private:
-        char* m_pBuffer;
-        uint32_t m_nSize;
-        char* volatile m_pHead;
-        char* volatile m_pTail;
+	inline char* HeadPtr() { return m_buffer + m_head; }
+	inline char* TailPtr() { return m_buffer + m_tail; }
+
+private:
+	char* m_buffer;
+	uint32_t m_size;
+	uint32_t volatile m_head;
+	uint32_t volatile m_tail;
 };
 
 #endif
